773f56b9aacd3099e6ba333e933681e2659b80a3
dannamayac <dxmayac@uqvirtual.edu.co>
2024-03-01 12:21:18 -0500

    cambio en navbar, se revierten los cambios 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,21 +3,30 @@ import { Link } from 'react-router-dom';
 import '../styles/NavbarStyle.css'
 
 const Navbar = () => {
-   const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+      setShowMenu(!showMenu);
+  };
 
   return (
     <nav className="navbar">
-      <div className="navbar-toggle" onClick={() => setShowMenu(!showMenu)}>
-        <i className="fas fa-bars"></i>
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        <i className="fa fa-bars"></i>
       </div>
       <ul className={`nav-list ${showMenu ? 'active' : ''}`}>
         <li className="nav-item">
-          <Link to="/" className="nav-link">Inicio</Link>
+          <Link to="/home" className="nav-link">Bancos</Link>
         </li>
         <li className="nav-item">
-          <Link to="/about" className="nav-link">Acerca de</Link>
+          <Link to="/accounts" className="nav-link">Cuentas</Link>
         </li>
-        {/* Agrega más elementos del menú según sea necesario */}
+        <li className="nav-item">
+          <Link to="/transactions" className="nav-link">Transacciones</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/settings" className="nav-link">Configuración</Link>
+        </li>
       </ul>
     </nav>
   );
