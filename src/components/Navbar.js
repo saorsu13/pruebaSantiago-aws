25d81e56520228f12d8ed4924f4df722c08c2997
dannamayac <dxmayac@uqvirtual.edu.co>
2024-03-01 12:19:40 -0500

    cambio en navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,28 +5,19 @@ import '../styles/NavbarStyle.css'
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
-  const toggleMenu = () => {
-      setShowMenu(!showMenu);
-  };
-
   return (
     <nav className="navbar">
-      <div className="navbar-toggle" onClick={toggleMenu}>
-        <i className="fa fa-bars"></i>
+      <div className="navbar-toggle" onClick={() => setShowMenu(!showMenu)}>
+        <i className="fas fa-bars"></i>
       </div>
       <ul className={`nav-list ${showMenu ? 'active' : ''}`}>
         <li className="nav-item">
-          <Link to="/home" className="nav-link">Bancos</Link>
+          <Link to="/" className="nav-link">Inicio</Link>
         </li>
         <li className="nav-item">
-          <Link to="/accounts" className="nav-link">Cuentas</Link>
+          <Link to="/about" className="nav-link">Acerca de</Link>
         </li>
-        <li className="nav-item">
-          <Link to="/transactions" className="nav-link">Transacciones</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/settings" className="nav-link">Configuración</Link>
-        </li>
+        {/* Agrega más elementos del menú según sea necesario */}
       </ul>
     </nav>
   );
